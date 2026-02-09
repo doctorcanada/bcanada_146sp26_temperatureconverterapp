f9cc9861726a2a2b6a074baee03d9a47cc9388e4
Doctor Canada <bcanada@uscb.edu>
2026-02-09 13:10:20 -0500

    wrote pseudocode comments, ready to start writing detailed code

--- src/TemperatureConverterApp.java
+++ src/TemperatureConverterApp.java
@@ -3,5 +3,45 @@
  * @author bcanada
  */
 public class TemperatureConverterApp {
+
+    public static void main(String[] args) {
+        /* INITIALIZATION PHASE */
+        // Create a Scanner to read in user input 
+        // Create an integer variable for store the user's menu choice
+        // (from the menu options listed below)
+
+        /* PROCESSING PHASE */
+        // Repeat until the user chooses to exit:
+        //   - Display a menu with three choices:
+        //       1) Convert Fahrenheit to Celsius
+        //       2) Convert Celsius to Fahrenheit
+        //       3) Exit
+        //   - Prompt the user to enter their choice and read it.
+        //
+        //   - If the choice is 1 or 2:
+        //       - Prompt the user to enter a temperature and read it (as an int).
+        //       - If choice is 1:
+        //           - Call the static `celsius` method to convert the value 
+        //             from F to C.
+        //           - Display a message showing the original Fahrenheit value 
+        //             and the converted Celsius value.
+        //       - Else if choice is 2:
+        //           - Call the static `fahrenheit` method to convert the value 
+        //             from C to F.
+        //           - Display a message showing the original Celsius value and 
+        //             the converted Fahrenheit value.
+        //   - If the choice is 3:
+        //       - Exit the loop.
+        
+        /* TERMINATION PHASE */
+        // Print a friendly goodbye message.
+        
+    } // end method main
+
+    // TODO: Write a static method named celsius that accepts an int Fahrenheit temperature
+    //       and returns the converted Celsius temperature as an int.
     
-}
+    // TODO: Write a static method named fahrenheit that accepts an int Celsius temperature
+    //       and returns the converted Fahrenheit temperature as an int.
+    
+} // end class TemperatureConverterApp
